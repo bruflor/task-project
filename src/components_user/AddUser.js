00bd05d271ddf9d6b21e0a06c3ab13422ef299e9
Bruna Flôr <brn.flor@gmail.com>
2022-08-15 21:57:29 +0100

    basic css card

--- src/components_user/AddUser.js
+++ src/components_user/AddUser.js
@@ -1,17 +1,22 @@
+import Card from "./cards/Card";
+import classes from "./AddUser.module.css";
+
 const AddUser = () => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text"></input>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text"></input>
 
-      <label htmlFor="age">Age (Years)</label>
-      <input id="age" type="number"></input>
-      <button type="submit">Add user</button>
-    </form>
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number"></input>
+        <button type="submit">Add user</button>
+      </form>
+    </Card>
   );
 };
 
